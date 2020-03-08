0bc1239ae0db0d7a969725d90fdf2cb1245d9294
Bram De Cneudt <bramdecneudt@gmail.com>
2020-03-08 11:43:15 +0100

    initial commit, still learning the ropes of powershell

---
+++ JavaVersionManager.ps1
@@ -0,0 +1,16 @@
+
+function java-get-list {
+    Get-Content -Path $PSScriptRoot\contents.txt -Encoding UTF8 -TotalCount 1
+    #get the contents
+    #write them into accessable objects
+    #list the objects
+
+    #EXTRA: output the path of the contents file
+}
+
+function java-select-version {
+    #output the list if empty
+    #for now do a temporary add to the path if a interger is inputted in the method
+        #later on add it to fixed to path
+        #later on replace the java path with new path
+}
