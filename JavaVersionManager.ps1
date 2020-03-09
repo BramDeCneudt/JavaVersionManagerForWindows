daf85892bc8b8c7435d4b36c58c8a11a19229a58
Bram De Cneudt <bramdecneudt@gmail.com>
2020-03-09 21:55:06 +0100

    getting custom objects from json file works, working on extracting the variables from the custom object

--- JavaVersionManager.ps1
+++ JavaVersionManager.ps1
@@ -1,16 +1,30 @@
 
-function java-get-list {
-    Get-Content -Path $PSScriptRoot\contents.txt -Encoding UTF8 -TotalCount 1
-    #get the contents
-    #write them into accessable objects
-    #list the objects
+function get-java-versions {
+    Get-Content -Path $PSScriptRoot\java-versions.json -Encoding UTF8 | ConvertFrom-Json
+}
 
-    #EXTRA: output the path of the contents file
+function java-get-list {
+    get-java-versions
+    #number the output
 }
 
 function java-select-version {
-    #output the list if empty
+   #Param(
+   #[parameter(Mandatory=$true)][Int] $number=0
+   #)
+   $array = get-java-versions
+   $java_version = $array[0]
+
+   # extract variable from java_version
+   $java_version | Get-Member -Name "path"
+   #$env:Path += ";"
     #for now do a temporary add to the path if a interger is inputted in the method
         #later on add it to fixed to path
         #later on replace the java path with new path
+
+ #output the list if empty
 }
+
+
+# use modules to privatize the functions you don't want to export
+#EXTRA: output the path of the contents file
