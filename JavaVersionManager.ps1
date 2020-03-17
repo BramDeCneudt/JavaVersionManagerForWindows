70147ef7ac103f56ebfd6f3150660eee1601997e
Bram De Cneudt <bramdecneudt@gmail.com>
2020-03-17 21:47:51 +0100

    added some todos, previous path and value is now saved to windows env, replacing previous java value that was filled in by this script with a current value now works

--- JavaVersionManager.ps1
+++ JavaVersionManager.ps1
@@ -1,8 +1,25 @@
+$prefix = "Wjvm_"
+
 function getObjectsFromFile {
     Param(
     [parameter(Mandatory=$true)][string] $fileName
     )
     Get-Content -Path $PSScriptRoot\$fileName -Encoding UTF8 | ConvertFrom-Json
+}
+
+function getEnvironmentVariable {
+    Param(
+    [parameter(Mandatory=$true)][string] $variableName
+    )
+    [Environment]::GetEnvironmentVariable($variableName, [System.EnvironmentVariableTarget]::User)
+}
+
+function setEnvironmentVariable {
+    Param(
+    [parameter(Mandatory=$true)][String] $variableName,
+    [parameter(Mandatory=$true)][String] $newValue
+    )
+   [Environment]::SetEnvironmentVariable($variableName, $newValue, [System.EnvironmentVariableTarget]::User)
 }
 
 
@@ -12,36 +29,39 @@ function get-java-versions {
 
 function java-get-list {
     get-java-versions
-
-}
-
-function get-settings {
-    getObjectsFromFile "settings.json"
-}
-
-function save-settings {
-#TODO implement further
 }
 
 function java-select-version {
    Param(
    [parameter(Mandatory=$true)][Int] $number
    )
-   #TODO set a first time check up here for getting the settings
    $array = get-java-versions
    $java_version = $array[$number]
    $name = $java_version.name
    $path = $java_version.path
-   #TODO backup used path to settings object
+
+   $oldPath = getEnvironmentVariable "Test"
+   $oldValue = getEnvironmentVariable $prefix"previousValue"
+
+   p $oldPath
+
+   $newValue = $oldPath + ";"+ $path
+
+   if (![String]::IsNullOrWhiteSpace($oldValue)) {
+        p "non empty value, seeing if it is contained in the path"
+        if ($oldPath.contains($oldValue)) {
+        p "oldvalue is contained in oldpath, replacing the value"
+                $newValue = $oldPath.replace($oldValue, $path)
+        } else  {
+            p "found oldvalue but was not contained in path"
+        }
+   }
 
 
-   $oldPath = [Environment]::GetEnvironmentVariable("Test", [System.EnvironmentVariableTarget]::User)
-   #TODO backup old path to settings object
-   p $oldPath.Contains($path)
-
-
-   [Environment]::SetEnvironmentVariable('Test', $oldPath + ";"+ $path, [System.EnvironmentVariableTarget]::User)
-
+   p $newValue
+   setEnvironmentVariable "Test" $newValue
+   setEnvironmentVariable $prefix"oldPath" $oldPath
+   setEnvironmentVariable $prefix"previousValue" $path
 }
 
 function p {
@@ -53,6 +73,11 @@ function p {
 
 function testground {
     echo ([Environment]::GetEnvironmentVariable("Test", [System.EnvironmentVariableTarget]::User))
+}
+
+function reloadEnv {
+    $env:Path = [System.Environment]::GetEnvironmentVariable("Path","Machine") + ";" + [System.Environment]::GetEnvironmentVariable("Path","User") 
+    $env:Test = getEnvironmentVariable "Test"
 }
 
 
